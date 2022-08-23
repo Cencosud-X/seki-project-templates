39acf0ed460a74eb434f9f97bbbd114fe38234a1
David Muñoz Gaete <david.munozgaete@cencosud.cl>
2022-08-23 13:32:56 -0400

    feat(rollback): added rollback scripts

--- api/scripts/rollback.js
+++ api/scripts/rollback.js
@@ -1,3 +1,15 @@
+const fs = require('fs');
+
 module.exports = (rc, files, error) => {
-  console.log('rollback', rc, files);
+
+  console.log('Executing rollback: ')
+  console.log(' ')
+  console.log('> Cleaning Monorepo....')
+  const projectOutputPath = path.join(rc.workspace_path, rc.group_folder, rc.path);
+  fs.rmdirSync(projectOutputPath, { recursive: true });
+
+  console.log("> Cleaning Monorepo....\u001b[2K\u001b[0E> Cleaning Monorepo.... ✅")
+  
+  console.log('> Rollback nx....')
+
 }
