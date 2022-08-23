003889558e55f607ac29d9752cb39c51938ed6ed
David Muñoz Gaete <david.munozgaete@cencosud.cl>
2022-08-23 13:39:21 -0400

    feat(fix): rollback

--- api/scripts/rollback.js
+++ api/scripts/rollback.js
@@ -3,18 +3,24 @@ const fs = require('fs');
 module.exports = (rc, files, error) => {
 
   return new Promise((resolve, reject) => {
-    console.log('> Cleaning Monorepo....')
+    try {
+      console.log('> Cleaning Monorepo....')
 
-    // Git clone (clone repo templates)
-    const child = spawn(`npx nx g @nrwl/workspace:rm ${rc.path}`, {
-      shell: true,
-      cwd: rc.workspace_path
-    })
+      // Git clone (clone repo templates)
+      const child = spawn(`npx nx g @nrwl/workspace:rm ${rc.path}`, {
+        shell: true,
+        cwd: rc.workspace_path
+      })
 
-    //spit stdout to screen
-    child.stdout.on('data', (d) => console.log(`${d.toString()}`))
-    child.stderr.on('data', (d) => console.log(chalk.red(`${d.toString()}`)))
-
-    child.on('close', (exitCode) => exitCode === 0 ? resolve() : reject(new Error('failed to rollback Nx')));
+      //spit stdout to screen
+      child.stdout.on('data', (d) => console.log(`${d.toString()}`))
+      child.stderr.on('data', (d) => console.log(chalk.red(`${d.toString()}`)))
+      child.on('close', (exitCode) => {
+        console.log('> Rollback ✅ DONE')
+        exitCode === 0 ? resolve() : reject(new Error('failed to rollback Nx'))
+      });
+    } catch (ex) {
+      reject(ex);
+    }
   })
 }
