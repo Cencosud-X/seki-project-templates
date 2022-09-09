da5c0de01743358966dddbf7f046e9cf8f359f5c
David Muñoz Gaete <david.munozgaete@cencosud.cl>
2022-09-09 13:26:16 -0300

    test(versbose): verbose path module

--- library/scripts/pre.js
+++ library/scripts/pre.js
@@ -17,6 +17,8 @@ module.exports = async (runner, args) => {
       // the nx.json file into the root project
       importPath = rc.path
 
+      console.debug(path);
+      console.debug(rc.workspacePath);
       const nxPath = path.join(rc.workspacePath, "nx.json");
       if (fs.existsSync(nxPath)) {
         const rawContent = fs.readFileSync(nxPath).toString("utf-8");
