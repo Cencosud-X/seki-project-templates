77a67ee96408c6a4a333fdbe9ab348ee41e073b2
David Muñoz Gaete <david.munozgaete@cencosud.cl>
2022-09-09 12:50:03 -0300

    feat(library): added library kind

---
+++ library/scripts/pre.js
@@ -0,0 +1,24 @@
+module.exports = async (runner, args) => {
+  try {
+    console.log('> PRE: Installing prerequisites (Library):');
+
+    const rc = args.rc;
+
+    // plubishable: a library marked as publishable (npm publish)
+    // buildable: a library marked as a "build" dist output
+    const isPublishable = rc.settings.publishable|false;
+    const isBuildable = rc.settings.buildable|false;
+
+    await runner.execute([
+      'npm install -D @nrwl/js@14.4.3',
+      `npx nx g @nrwl/js:lib ${rc.path} ${isPublishable ? '--publishable' : ''} ${isBuildable ? '--buildable' : ''}`
+    ], {
+      cwd: rc.workspacePath
+    })
+
+    console.log('> PRE: requisites ✅ DONE')
+
+  } catch {
+    throw new Error('failed to install library pre-requisites');
+  }
+}
