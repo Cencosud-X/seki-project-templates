b0c9e3ff50e9d5272008ebcbbb00f247bae56ea2
David Muñoz Gaete <david.munozgaete@cencosud.cl>
2022-09-12 11:48:18 -0300

    feat(projects): adding web settings

--- web/scripts/post.js
+++ web/scripts/post.js
@@ -1,6 +1,12 @@
 module.exports = async (runner, args) => {
   try {
     console.log('> POST: Cleansing (WEB):');
+
+    const rc = args.rc;
+    await runner.execute(`npx nx g @nrwl/workspace:rm ${rc.path}-e2e`, {
+      cwd: rc.workspace_path
+    })
+
 /*
     await runner.execute([
       'rm -rf ./src/app',
