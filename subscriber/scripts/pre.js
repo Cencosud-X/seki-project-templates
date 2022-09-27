aed4b689659816e36c09d5dec9d7da59c6895674
kmilo8346 <kmilo8346@gmail.com>
2022-09-27 17:21:19 -0300

    feat(pre): updated streamer lib

--- subscriber/scripts/pre.js
+++ subscriber/scripts/pre.js
@@ -1,20 +1,22 @@
 module.exports = async (runner, args) => {
   try {
-    console.log('> PRE: Installing prerequisites (Subscriber):');
+    console.log("> PRE: Installing prerequisites (Subscriber):");
 
     const rc = args.rc;
-    await runner.execute([
-      'npm install --save-dev @nrwl/nest@14.4.3',
-      'npm install --save @team_seki/streamer@0.0.9',
-      `npx nx g @nrwl/nest:app ${rc.path}`,
-    ], {
-      cwd: rc.workspace_path
-    });
+    await runner.execute(
+      [
+        "npm install --save-dev @nrwl/nest@14.4.3",
+        "npm install --save @team_seki/streamer@0.0.10",
+        `npx nx g @nrwl/nest:app ${rc.path}`,
+      ],
+      {
+        cwd: rc.workspace_path,
+      }
+    );
 
-    console.log('> PRE: requisites ✅ DONE');
-
+    console.log("> PRE: requisites ✅ DONE");
   } catch {
     console.error(ex);
-    throw new Error('Failed to install subscriber pre-requisites');
+    throw new Error("Failed to install subscriber pre-requisites");
   }
-}
+};
