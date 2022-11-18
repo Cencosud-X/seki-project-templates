bce8f4cfd2c7683e6705104b68284b5c83cc3d51
Maykel Guzmán Carvajal <maykelg2005@gmail.com>
2022-11-18 07:50:35 -0500

    chore(library) change add tags in pre script

--- library/scripts/pre.js
+++ library/scripts/pre.js
@@ -11,30 +11,18 @@ module.exports = async (runner, args) => {
     // buildable: a library marked as a "build" dist output
     const isPublishable = rc.settings.publishable | false;
     const isBuildable = rc.settings.buildable | false;
-    let importPath = '';
+    let publishableArg = '';
     if (isPublishable) {
       const npmOrganization = rc.settings.npm.organization;
-      importPath = `@${npmOrganization}/${rc.path}`;
+      const importPath = `@${npmOrganization}/${rc.path}`;
+      publishableArg = `--publishable --importPath=\"${importPath}\" --tags=\"REQUIRED:GOLDEN\"`
     }
     await runner.execute([
       'npm install -D @nrwl/js@14.4.3',
-      `npx nx g @nrwl/js:lib ${rc.path} ${isPublishable ? `--publishable --importPath=\"${importPath}\"` : ''} ${isBuildable ? '--buildable' : ''}`
+      `npx nx g @nrwl/js:lib ${rc.path} ${publishableArg} ${isBuildable ? '--buildable' : ''}`
     ], {
       cwd: rc.workspace_path
     })
-    
-    if(isPublishable) {
-      const projectJsonPath = path.join(args.workspacePath, "project.json");
-      if (fs.existsSync(projectJsonPath)) {
-        const rawContent = fs.readFileSync(projectJsonPath).toString("utf-8");
-        const parsedContent = JSON.parse(rawContent);
-        if(Array.isArray(parsedContent.tags)) {
-          parsedContent.tags.push("REQUIRED:GOLDEN")  
-        }
-        const newContent = JSON.stringify(parsedContent, null, 2);
-        fs.writeFileSync(projectJsonPath, newContent, { flag: fs.constants.O_WRONLY })
-      }
-    }
 
     console.log('> PRE: requisites ✅ DONE')
 
