faed4f71de4eb70ea0cc5a7463aecc02ae12a89d
Ernesto Fuentes <ernesto.fuentesgomez@gmail.com>
2022-11-24 18:42:21 -0300

    feat(api_koa): update template to load basic app

--- api_koa/scripts/pre.js
+++ api_koa/scripts/pre.js
@@ -1,18 +1,22 @@
 module.exports = async (runner, args) => {
   try {
-    console.log('> PRE: Installing prerequisites (API):');
+    console.log("> PRE: Installing prerequisites (API):");
 
     const rc = args.rc;
-    await runner.execute([
-      `nx g @nrwl/node:application ${rc.path}`
-    ], {
-      cwd: rc.workspace_path
-    })
+    await runner.execute(
+      [
+        `nx g @nrwl/node:application ${rc.path}`,
+        "npm i koa koa-bodyparser koa-router",
+        "npm i -D @types/koa @types/koa-bodyparser @types/koa-router",
+      ],
+      {
+        cwd: rc.workspace_path,
+      }
+    );
 
-    console.log('> PRE: requisites ✅ DONE')
-
+    console.log("> PRE: requisites ✅ DONE");
   } catch {
     console.error(ex);
-    throw new Error('failed to install api pre-requisites');
+    throw new Error("failed to install api pre-requisites");
   }
-}
+};
