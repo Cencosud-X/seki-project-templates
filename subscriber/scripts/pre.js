728a4067685b88371fa65fa78b1d0e3fdff6d6ba
kmilo8346 <kmilo8346@gmail.com>
2022-12-15 15:47:54 -0300

    feat(subscriber): bump versions of plugins

--- subscriber/scripts/pre.js
+++ subscriber/scripts/pre.js
@@ -5,9 +5,9 @@ module.exports = async (runner, args) => {
     const rc = args.rc;
     await runner.execute([
       'npm install --save-dev @nrwl/nest@14.4.3',
-      'npm install --save @team_seki/subscriber-plugin@1.0.4',
-      'npm install --save @team_seki/kafka-streamer-plugin@1.0.2',
-      'npm install --save @team_seki/pubsub-streamer-plugin@1.0.8',
+      'npm install --save @team_seki/subscriber-plugin@1.0.8',
+      'npm install --save @team_seki/kafka-streamer-plugin@1.0.6',
+      'npm install --save @team_seki/pubsub-streamer-plugin@1.0.15',
       `npx nx g @nrwl/nest:app ${rc.path}`,
     ], {
       cwd: rc.workspace_path
