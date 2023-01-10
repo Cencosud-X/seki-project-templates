233d5f902c7b285c799c725937a0ac456097c806
David Muñoz Gaete <david.munozgaete@cencosud.cl>
2023-01-10 17:36:28 -0300

    feat(removed): post script for web

--- web/scripts/post.js
+++ web/scripts/post.js
@@ -8,6 +8,9 @@ module.exports = async (runner, args) => {
       'rm -rf ./src/app',
       'rm -rf ./src/test-setup.ts',
       'rm -rf ./favicon.ico',
+      'rm -rf ./src/favicon.ico',
+      'rm -rf ./src/main.ts',
+      'rm -rf ./src/style.css',
       //'rm -rf ./src/assets',
       'rm -rf ./src/environments',
     ], {
